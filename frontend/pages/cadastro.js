943dd45eabfff4547365fe53c563df729a383a61
EA - Giovanna Totte Coutinho <giovanna_totte@educadventista.org>
2025-11-06 07:44:31 -0300

    atualização dos arquivos

--- frontend/pages/cadastro.js
+++ frontend/pages/cadastro.js
@@ -1,24 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('button');
-    button.onclick = (event) => {
-        event.preventDefault();
-        sendUser();
+const button = document.querySelector("button")
+
+button.onclick = (event) => {
+    event.preventDefault()
+    sendUser()
+}
+
+async function sendUser() {
+    const name = document.querySelector("#nome").value
+    const email = document.querySelector("#email").value
+    const password = document.querySelector("#senha").value
+
+    const user = {
+        name,
+        email,
+        password
     }
-});
 
-function sendUser() {
-    const name = document.querySelector("#nome").value;
-    const email = document.querySelector("#email").value;
-    const password = document.querySelector("#senha").value;
-
-    const user = { name, email, password };
-    console.log(user);
-
-    fetch("http://localhost:3333/cadastrar", {
+    const response = await fetch("http://localhost:3333/cadastrar", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({ user })
-    })
+    }).then(response => response.json())
+
+    alert(response.message)
+
+    window.location.href = "../index.html"
 }
